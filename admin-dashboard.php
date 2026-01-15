f53f45798a90ee9e02f849349cdf31f3f13ebe47
Frederick Rosales <iamfrederickr@gmail.com>
2026-01-15 12:42:56 +0800

    Add input validation for quantity field in edit item form

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -943,7 +943,8 @@ if (isset($_GET['edit_item'])) {
                             <div class="col-6 mb-3">
                                 <label class="form-label fw-bold">Quantity</label>
                                 <input type="number" name="quantity" class="form-control" min="0" max="100"
-                                    value="<?php echo $edit_item['quantity'] ?? 1; ?>" required>
+                                    value="<?php echo $edit_item['quantity'] ?? 1; ?>"
+                                    oninput="if(this.value > 100) this.value = this.value = 100;" required>
                             </div>
 
                             <div class="col-6 mb-3">
